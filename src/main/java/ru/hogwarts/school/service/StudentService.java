cd2de16bc1c80902a71da88a874d02655eb2355a
alexterkin <alexman.terkin@gmail.com>
2025-07-26 23:15:32 +0300

    Домашка Введение в БД

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -2,41 +2,38 @@ package ru.hogwarts.school.service;
 
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 
 @Service
 public class StudentService {
-    private final HashMap<Long, Student> students = new HashMap<>();
-    private long count = 0;
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     public Student addStudent(Student student) {
-        student.setId(count++);
-        students.put(student.getId(), student);
-        return student;
+        return studentRepository.save(student);
     }
 
     public Student findStudent(long id) {
-        return students.get(id);
+        return studentRepository.getReferenceById(id);
     }
 
     public Student editStudent(Student student) {
-        if(students.containsKey(student.getId())) {
-            students.put(student.getId(), student);
-            return student;
-        }
-        return null;
+        return studentRepository.save(student);
     }
 
     public void deleteStudent(long id) {
-        students.remove(id);
+        studentRepository.deleteById(id);
     }
 
     public Collection<Student> findByAge(int age) {
         ArrayList<Student> result = new ArrayList<>();
-        for(Student student : students.values()) {
+        for(Student student : studentRepository.findByAge(age)) {
             if(student.getAge() == age) {
                 result.add(student);
             }
